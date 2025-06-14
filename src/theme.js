51b777020e035334c382792c94df948eea96ab50
phamlong <phamlong123np@gmail.com>
2025-06-14 21:52:47 +0700

    Basic Layout

--- src/theme.js
+++ src/theme.js
@@ -3,10 +3,16 @@ import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
+  // tự tạo ra thuộc tính
+  trello: {
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
         // máy thằng button mặt đinh nó sẽ là màu primary
+        // màu main sẽ là teal còn light nó nhẹ hơn,dark nó nặng hơn
         primary: teal,
         // màu này đc dùng khi các component có màu secondary
         secondary:deepOrange
