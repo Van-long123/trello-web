c5ef18eb2c0ce1514fb99a693f69d313c8e0cce1
phamlong <phamlong123np@gmail.com>
2025-06-18 21:44:30 +0700

    Trello Board Content

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -100,7 +100,10 @@ function BoardBar() {
                 width: '34px',
                 height: '34px',
                 fontSize: '16px',
-                border: 'none'
+                border: 'none',
+                color: 'white',
+                cursor: 'pointer',
+                '&:first-of-type': { bgcolor: '#a4b0be' }
               }
             }}
           >
