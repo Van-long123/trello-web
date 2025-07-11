5dbc768583029b4dbd092916681a484f47d5f940
phamlong <phamlong123np@gmail.com>
2025-07-11 15:23:52 +0700

    Add New Column And Card Part 4

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -5,13 +5,21 @@ import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
 import { fetchBoardDetailsAPI, createNewColumnAPI, createNewCardAPI } from '~/apis'
-
+import { generatePlaceholderCard } from '~/utils/formatters'
+import { isEmpty, cloneDeep } from 'lodash'
 function Board() {
   const [board, setBoard] = useState(null)
   useEffect(() => {
     //tạm thời fix cố định boardId
     const boardId = '686a993347b0301e61c873b4'
     fetchBoardDetailsAPI(boardId).then((board) => {
+      // Cần xử lý vấn đề kéo thả vào một column rỗng
+      board.columns.forEach(column => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)]
+          column.cardOrderIds = [generatePlaceholderCard(column)._id]
+        }
+      })
       setBoard(board)
     })
     // console.log(board)
@@ -23,7 +31,15 @@ function Board() {
       ...newColumnData,
       boardId: board._id
     })
+
+    createdColumn.cards = [generatePlaceholderCard(createdColumn)]
+    createdColumn.cardOrderIds = [generatePlaceholderCard(createdColumn)._id]
+
     //Cập nhật lại state board
+    const newBoard = cloneDeep(board)
+    newBoard.columns.push(createdColumn)
+    newBoard.columnOrderIds.push(createdColumn._id)
+    setBoard(newBoard)
   }
 
   // Func này có nhiệm vụ gọi API tạo mới Card và làm lại dữ liệu State Board
@@ -34,6 +50,14 @@ function Board() {
     })
 
     //Cập nhật lại state board
+    const newBoard = cloneDeep(board)
+    const columnToUpdate = newBoard.columns.find(column => column._id === createdCard.columnId)
+    if (columnToUpdate) {
+      columnToUpdate.cards.push(createdCard)
+      columnToUpdate.cardOrderIds.push(createdCard._id)
+    }
+    setBoard(newBoard)
+
   }
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
